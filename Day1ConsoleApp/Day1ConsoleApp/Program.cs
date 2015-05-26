97b9622e7b6b92fdb0a28f5ad7c1d579ada499b6
magicRawb <magicbyrob@gmail.com>
2015-05-26 18:01:08 -0500

    Update

--- Day1ConsoleApp/Day1ConsoleApp/Program.cs
+++ Day1ConsoleApp/Day1ConsoleApp/Program.cs
@@ -14,7 +14,7 @@ namespace Day1ConsoleApp
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             for (int i = 0; i <= 100; i++)
             {
                 if (i % 7 == 0)
