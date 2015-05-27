9c8315c13134af83391f0b35d5899e83ff59e918
magicRawb <magicbyrob@gmail.com>
2015-05-27 17:09:14 -0500

    Day 2 Lab 7 Initial Commit

---
+++ Day2Lab7/Day2Lab7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cleaned = Utility.CleanUserInput("   aswefae    ");
+
+            Console.WriteLine("Please type any sentence.");
+            Counter.WordCount(Console.ReadLine());
+                
+        }
+    }
+}
---
+++ Day2Lab7/Day2Lab7/Utility.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day2Lab7
+{
+    class Utility
+    {
+        public static string CleanUserInput(string input)
+        {
+            return input.ToLower().Trim();
+        }
+    }
+
+
+    public class Product
+    {
+        public decimal Price { get; set; }
+
+        public static Product Create(decimal price)
+        {
+            return new Product();
+        }
+    }
+
+    public class Counter
+    {
+        public static Counter WordCount(string userInput)
+        {
+            var array = userInput.Split(' ');
+        }
+    }
+}
