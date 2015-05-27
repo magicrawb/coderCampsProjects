9f410451410d8549806da701d887cd819791638b
magicRawb <magicbyrob@gmail.com>
2015-05-26 23:24:04 -0500

    Almost finished. Need to add network connection failure catch.

--- PlayMovieQuotesHW1/PlayMovieQuotesHW1/Program.cs
+++ PlayMovieQuotesHW1/PlayMovieQuotesHW1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,23 @@ namespace PlayMovieQuotesHW1
     {
         static void Main(string[] args)
         {
+            SoundPlayer words = new SoundPlayer(@"C:\CoderCampsProjects\coderCampsProjects\PlayMovieQuotesHW1\PlayMovieQuotesHW1\dead_poets_ideas.wav");
+            SoundPlayer aladdin = new SoundPlayer(@"C:\CoderCampsProjects\coderCampsProjects\PlayMovieQuotesHW1\PlayMovieQuotesHW1\aladdin_outta_here.wav");
+            SoundPlayer vietnam = new SoundPlayer(@"C:\CoderCampsProjects\coderCampsProjects\PlayMovieQuotesHW1\PlayMovieQuotesHW1\good_morning.wav");
+            Random rdm = new Random();
+            int randomNumber = rdm.Next(1, 4);
+            switch (randomNumber)
+            {
+                case 1:
+                    words.PlaySync();
+                    break;
+                case 2:
+                    aladdin.PlaySync();
+                    break;
+                case 3:
+                    vietnam.PlaySync();
+                    break;
+            }
         }
     }
 }
