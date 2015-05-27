4d27f414a4b7ba3d8c822dc36492dda8b29792bc
magicRawb <magicbyrob@gmail.com>
2015-05-27 17:09:48 -0500

    Object Oriented Programming
    Day 2 Initial Commit

---
+++ OOPDay/OOPDay/Product.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDay
+{
+    // Product Class
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string PriceString
+        {
+            get { return Price.ToString("C"); }
+        }
+
+        public decimal CalculatePrice()
+        {
+            return CalculatePrice(0, 0);
+        }
+
+        public decimal CalculatePrice(decimal taxRate, decimal discount)
+        {
+            return this.Price + (this.Price * taxRate) - (this.Price * discount);
+        }
+
+        public Product(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public Product()
+        {
+
+        }
+
+    }
+    // End Product Class
+    // Customer Class
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        private int _age;
+
+        public int Age
+        {
+            get 
+            { 
+                return _age; 
+            }
+            set 
+            { 
+                _age = value < 0 ? 0 : value;
+            }
+        }
+        
+        public int Birthdate { get; set; }
+
+        public Customer(string first, string last, int age, int birthdate)
+        {
+            this.FirstName = first;
+            this.LastName = last;
+            this.Age = age;
+            this.Birthdate = birthdate;
+        }
+    }
+    // End Customer Class
+    // AddNumbers Class
+    public class Math
+    {
+        public int AddNumbers(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        
+        public int AddNumbers(params int[] numbers)
+        {
+            int result = 0;
+            foreach (var num in numbers)
+            {
+                result += num;
+            }
+
+            return result;
+        }
+
+    }
+    // End AddNumbers Class
+}
--- OOPDay/OOPDay/Program.cs
+++ OOPDay/OOPDay/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,34 @@ namespace OOPDay
     {
         static void Main(string[] args)
         {
+            Product robsLaptop = new Product();
+            robsLaptop.Name = "Acer";
+            robsLaptop.Price = 350.75m;
+
+            robsLaptop.CalculatePrice();
+
+            Console.WriteLine("Name: {0}, Price: {1}", robsLaptop.Name, robsLaptop.PriceString);
+            
+            
+            // Begin Customer Creation
+            Customer sally = new Customer("Sally", "Williams", 23, 2028);
+            Customer mike = new Customer("Mike", "Harrison", 10, 2019);
+
+            if (sally.Age == 23)
+            {
+                Console.WriteLine("Sally is 23");
+            }
+            
+            if (mike.Age == 10)
+            {
+                Console.WriteLine("Mike is 10");
+            }
+            // End Customer Creation
+
+            int addThem = Math.AddNumbers(10, 12);
+
+
+            Console.ReadLine();
         }
     }
 }
