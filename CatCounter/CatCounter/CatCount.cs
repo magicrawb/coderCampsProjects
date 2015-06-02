fc985c0ce3029e9987a0a8b4d46bc03091bf52af
magicRawb <magicbyrob@gmail.com>
2015-06-01 22:36:12 -0500

    Cat Counter HW initial commit

---
+++ CatCounter/CatCounter/CatCount.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatCounter
+{
+    public class CatCount
+    {
+        public int Counter { get; set; }
+
+        public static int CatCountMethod(string s)
+        {
+            string[] cats = s.StripPunctuation().Split(' ');
+            CatCount counter = new CatCount();
+            counter.Counter = 0;
+
+            foreach (string word in cats)
+            {
+                string w = word.ToLower();
+                if (w == "cat")
+                {
+                    counter.Counter++;
+                }
+                if (w == "cats")
+                {
+                    counter.Counter++;
+                }
+                else
+                {
+
+                }
+
+            }
+
+            return counter.Counter;
+
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string StripPunctuation(this string s)
+        {
+            var sb = new StringBuilder();
+            foreach (char c in s)
+            {
+                if (!char.IsPunctuation(c))
+                    sb.Append(c);
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ CatCounter/CatCounter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Copy and Paste ANY block of text - I'll tell you how many cats it contains!");
+            String catString = Console.ReadLine();
+            Console.WriteLine("The word 'Cat' appears {0} times!", CatCount.CatCountMethod(catString));
+
+            Console.ReadLine();
+        }
+    }
+}
